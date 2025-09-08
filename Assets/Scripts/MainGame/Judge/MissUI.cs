d774fdfe1117be8acc4c6c6262d877e7af7fa242
atenai <alfamod47@yahoo.co.jp>
2025-09-09 01:32:34 +0900

    ミスした際のアニメーションをついか

--- Assets/Scripts/MainGame/Judge/MissUI.cs
+++ Assets/Scripts/MainGame/Judge/MissUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class MissUI : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class MissUI : MonoBehaviour
 	public void SetMissCount(int missCount)
 	{
 		this.missCount.text = missCount.ToString();
+		this.missCount.transform.DOShakeScale(0.5f, 1, 10, 90, false).SetEase(Ease.Linear);
 	}
 
 	/// <summary>
