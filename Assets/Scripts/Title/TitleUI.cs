b33454bc54c8f46df1282dec6824947922b6c02f
atenai <alfamod47@yahoo.co.jp>
2025-09-09 00:48:25 +0900

    DoTweenを追加

--- Assets/Scripts/Title/TitleUI.cs
+++ Assets/Scripts/Title/TitleUI.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
 
 public class TitleUI : MonoBehaviour
 {
+	[SerializeField] TextMeshProUGUI titleText;
 	[SerializeField] private Button playButton;
+	[SerializeField] private TextMeshProUGUI playButtonText;
 
 	[SerializeField] private Image difficultyImage;
 	[SerializeField] private Button easyButton;
+	[SerializeField] private TextMeshProUGUI easyButtonText;
 	[SerializeField] private Button normalButton;
+	[SerializeField] private TextMeshProUGUI normalButtonText;
 	[SerializeField] private Button hardButton;
+	[SerializeField] private TextMeshProUGUI hardButtonText;
 
 	public Button EasyButton => easyButton;
 	public Button NormalButton => normalButton;
@@ -23,6 +30,9 @@ public class TitleUI : MonoBehaviour
 	{
 		difficultyImage.gameObject.SetActive(false);
 		playButton.onClick.AddListener(OnClickPlay);
+
+		titleText.DOText("NumberPlace(Sudoku)", 1, scrambleMode: ScrambleMode.All).SetEase(Ease.Linear);
+		playButtonText.DOText("Play", 1, scrambleMode: ScrambleMode.All).SetEase(Ease.Linear);
 	}
 
 	/// <summary>
@@ -30,6 +40,10 @@ public class TitleUI : MonoBehaviour
 	/// </summary>
 	private void OnClickPlay()
 	{
+		playButton.gameObject.SetActive(false);
 		difficultyImage.gameObject.SetActive(true);
+		easyButtonText.DOText("Easy", 1, scrambleMode: ScrambleMode.All).SetEase(Ease.Linear);
+		normalButtonText.DOText("Normal", 1, scrambleMode: ScrambleMode.All).SetEase(Ease.Linear);
+		hardButtonText.DOText("Hard", 1, scrambleMode: ScrambleMode.All).SetEase(Ease.Linear);
 	}
 }
